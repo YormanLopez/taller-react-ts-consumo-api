620125c8dfc0567c7ba9826c18e6bff68bba91bc
Alvaro Pelaez <palvaroo950@gmail.com>
2026-09-19 02:32:30 -0500

    Prueba de las tarjetas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import TarjetaElemento from './components/tarjetaElemento.tsx'
+import ListaElementos from './components/listaElementos'
+const obras = [
+  { id: 1, titulo: 'La persistencia de la memoria', artista: 'Salvador Dalí', fecha: '1931' },
+  { id: 2, titulo: 'La noche estrellada', artista: 'Vincent van Gogh', fecha: '1889' }, 
+  { id: 3, titulo: 'El grito', artista: 'Edvard Munch', fecha: '1893' },
+  { id: 4, titulo: 'Las meninas', artista: 'Diego Velázquez', fecha: '1656' },
+];
+
 function App() {
   return (
     <>
       <h1>Galería de arte</h1>
-      <TarjetaElemento titulo="Pintura 1" artista="Artista 1" fecha="2023-01-01" />
+      <ListaElementos obras={obras} />
     </>
   )
 }
